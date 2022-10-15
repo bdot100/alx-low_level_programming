20facc1473c9e371852990c5c0032e9b77ad70d4
“Okunade <bidemiokunade@gmail.com>
2022-10-15 13:42:23 -0700

    Mandatory tasks done

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Prints alphabets in lower case followed by a new line
+ * Return: Always 0
+ */
+int main(void)
+{
+	char la;
+
+	for (la = 'a'; la <= 'z'; la++)
+	{
+		if (la != 'e' && la != 'q')
+			putchar(la);
+	}
+
+	putchar('\n');
+	return (0);
+}
