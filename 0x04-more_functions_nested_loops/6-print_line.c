a2673803a2f16e0290136446941977a5c5c90fff
“Okunade <bidemiokunade@gmail.com>
2022-10-20 09:16:09 -0700

    All tasks done plus mandatory tasks

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,20 +1,26 @@
 #include "main.h"
 
 /**
-*print_line - draws a straight line in the terminal.
-*
-*Return: lines 
-*/
+  * print_line - Draws a straight line according to number inputted
+  * @n: The number of lines to draw
+  *
+  * Return: empty
+  */
 void print_line(int n)
 {
-    int i;
+	int x;
 
-    if (n > 0)
-    {
-        for (i = 0; i <= n; i++)
-        {
-            _putchar('_');
-        }
-    }
-    _putchar('\n');
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (x = 0; x < n; x++)
+		{
+			_putchar(95);
+		}
+
+		_putchar('\n');
+	}
 }
