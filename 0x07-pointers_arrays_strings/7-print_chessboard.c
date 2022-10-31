24fbbe0a95fff74393b28e25b96ad6964c81ee0d
“Okunade <bidemiokunade@gmail.com>
2022-10-31 16:47:36 -0700

    task 7 done

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,24 +1,22 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * *print_chessboard - description
- * @a: board
- * Return: void
+ * print_chessboard - prints the chessboard
+ * @a: input pointer.
+ * Return: void.
  */
-
-
 void print_chessboard(char (*a)[8])
 {
-int i, j;
+	unsigned int i, j = 0;
 
-for (i = 0; i < 8; i++)
-{
-	for (j = 0; j < 8; j++)
+	for (i = 0; i < 64; i++)
 	{
-		_putchar(a[i][j]);
+		if (i % 8 == 0 && i != 0)
+		{
+			j = i;
+			_putchar('\n');
+		}
+		_putchar(a[i / 8][i - j]);
 	}
 	_putchar('\n');
-}
-
 }
