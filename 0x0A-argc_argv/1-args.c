70bf09beaf40448320db5222e8149fff9da7833a
“Okunade <bidemiokunade@gmail.com>
2022-11-08 14:29:09 -0800

    Task one done

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+  * main - Prints the no of args passed into the program
+  * @argc: argument count
+  * @argv: argument vector
+  *
+  * Return: Always zero
+  */
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%d\n", argc);
+
+	return (0);
+}
