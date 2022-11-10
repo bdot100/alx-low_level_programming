d9ea452dbac8683405e386c47e75f02f3f54006f
“Okunade <bidemiokunade@gmail.com>
2022-11-10 04:41:56 -0800

    task one

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * malloc_checked - allocates memory using malloc
+ * @b: number of memory to be allocated
+ * Return: void
+*/
+void *malloc_checked(unsigned int b)
+{
+    void *p;
+    p = malloc(b);
+    if (p == NULL)
+    {
+        exit(98);
+    }
+    return (p);
+}
---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,3 @@
+0x0C. C - More malloc, free
+This study/project is about memory allocation in C
+
